43c9f7a3f277952cb2d0f4ff4c6340cf53915492
Patricia <wambuipatricia3@gmail.com>
2023-03-30 20:12:47 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+
+// Parent class
+class Polygon {
+    constructor(sides) {
+      this.sides = sides;
+    }
+  
+    get countSides() {
+      return this.sides.length;
+    }
+  
+    get perimeter() {
+      return this.sides.reduce((acc, side) => acc + side);
+    }
+  }
+  
+  // Child class 1 - Triangle
+  class Triangle extends Polygon {
+    get isValid() {
+      if (this.countSides !== 3) return false;
+      const [a, b, c] = this.sides;
+      return (a + b > c) && (a + c > b) && (b + c > a);
+    }
+  }
+  
+  // Child class 2 - Square
+  class Square extends Polygon {
+    get isValid() {
+      if (this.countSides !== 4) return false;
+      const [a, b, c, d] = this.sides;
+      return a === b && b === c && c === d;
+    }
+  
+    get area() {
+      const [a, b] = this.sides;
+      return a * b;
+    }
+  }
